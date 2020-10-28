2ccb125acbcc928832f62dff69ac484cb4a54a6f
Poundenote <rayyan.naz.02@gmail.com>
2020-10-28 12:41:36 +0000

    Added comments and removed redudant class

--- SteamScrape.py
+++ SteamScrape.py
@@ -1,34 +1,36 @@
 from bs4 import BeautifulSoup
 import requests
 
-'''
-returns data in dictionary
-from key = title
-vale = (image, discount, discounted price)
-'''
-class SteamScrape:
 
-    def nl_stripper():
-        pass
+def get_data():
 
-    def get_data():
-        data = {}
-        url = "https://store.steampowered.com/search/?specials=1"
-        html_doc = requests.get(url)
-        soup = BeautifulSoup(html_doc.content, 'html.parser')
-        search_resultsRows = soup.find('div', {"id": "search_resultsRows"})
-        href = search_resultsRows.find_all(href = True)
-        for item in href:
-            title = ((item.find('span', {"class": "title"})).contents)[0]
-            image = ((item.find('div', {"class": "col search_capsule"})).img)['src']
-            discount = ((item.find('div', {"class": "col search_discount responsive_secondrow"})).text).strip()
-            discounted_price_div = (item.find('div', {"class": "col search_price discounted responsive_secondrow"}))
-            if discounted_price_div is not None:
-                discounted_price = str((discounted_price_div.contents[len(discounted_price_div)-1]).strip())
-            else:
-                discounted_price = "N/A"
-            data[title] = (image, discount, discounted_price)
-        return data
+    data = {}
+    url = "https://store.steampowered.com/search/?specials=1"
+    # pulls the html from the website to parsed by bs4
+    html_doc = requests.get(url)
+
+    # scrapes the url for all steam sales
+    soup = BeautifulSoup(html_doc.content, 'html.parser') 
+    search_results_rows = soup.find('div', {"id": "search_resultsRows"})
+    href = search_resultsRows.find_all(href = True)
+
+    # searches the href tag and finds the title, image, and discounts on all items in the sale
+    for item in href:
+        title = ((item.find('span', {"class": "title"})).contents)[0]
+        image = ((item.find('div', {"class": "col search_capsule"})).img)['src']
+        discount = ((item.find('div', {"class": "col search_discount responsive_secondrow"})).text).strip()
+        discounted_price_div = (item.find('div', {"class": "col search_price discounted responsive_secondrow"}))
+    
+        # some items in the discount list arent actually discounted this removes all non discounted items
+        if discounted_price_div is not None:
+            discounted_price = str((discounted_price_div.contents[len(discounted_price_div)-1]).strip())
+        else:
+            discounted_price = "N/A"
+
+        # sorts items into dictionary with tile as the key and tuple value
+        data[title] = (image, discount, discounted_price)
+    return data
+
 
 if __name__ == "__main__":
-    print(SteamScrape.get_data())
+    print(get_data())
